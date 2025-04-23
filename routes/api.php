24ae889454e6b0958cf5a30ce5b5899bee5216e4
Ahmad Halimi <haliminurja2017@gmail.com>
2025-04-23 23:24:44 +0700

    Redis

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\EnkripsiController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MailerController;
+use App\Http\Controllers\RedisController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,6 +19,16 @@ Route::prefix('mahasiswa')->middleware('auth:sanctum')->group(function () {
     Route::get('/{nim}', [MahasiswaController::class, 'show']);
     Route::put('/{nim}', [MahasiswaController::class, 'update']);
     Route::delete('/{nim}', [MahasiswaController::class, 'destroy']);
+});
+
+Route::prefix('redis')->group(function () {
+    Route::post('set', [RedisController::class, 'set']);
+    Route::get('get/{key}', [RedisController::class, 'get']);
+    Route::delete('delete/{key}', [RedisController::class, 'delete']);
+    Route::get('exists/{key}', [RedisController::class, 'exists']);
+    Route::post('flush', [RedisController::class, 'flushAll']);
+    Route::get('handle-cache', [RedisController::class, 'handleWithCache']);
+    Route::post('delete-by-pattern', [RedisController::class, 'deleteByPattern']);
 });
 
 Route::post('encrypt', [EnkripsiController::class, 'encrypt']);
